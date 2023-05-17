99bcb795a5e896258ab32c78cfa60939d73960e8
beratukiqi <berat.u@live.se>
2023-05-17 11:09:49 +0200

    Allt t.om. uppgift 4 klart

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const fs = require("fs");
+const readlineSync = require("readline-sync");
+
 // JSON.parse() - Skriver om till vanlig JS.
 // .toString() - Skriver om till sträng.
-const content =
-    "Why, sometimes I've believed as many as six impossible things before breakfast.";
+// const content =
+//     "Why, sometimes I've believed as many as six impossible things before breakfast.";
 
 // Uppgift 1
-fs.writeFileSync("alicequotes.txt", content);
+// fs.writeFileSync("alicequotes.txt", content);
 
-// Uppgift 2
-let text = fs.readFileSync("alicequotes.txt");
-text = text.toString();
-console.log(text);
+// // Uppgift 2
+// let text = fs.readFileSync("alicequotes.txt");
+// text = text.toString();
+// console.log(text);
+
+// // Uppgift 3
+// let css = fs.readFileSync("style.css");
+// css = css.toString();
+
+// let count = 0;
+
+// for (let i = 0; i < css.length; i++) {
+//     let currentChar = css[i];
+
+//     if (currentChar === "#") {
+//         count++;
+//     }
+// }
+
+// console.log(count, "number of ID's");
+
+// Uppgift 4.
+// let fileName = readlineSync.question("Enter filename: ");
+// let fileContent = readlineSync.question("Enter the content for the file: ");
+
+// fs.writeFileSync(`${fileName}`, fileContent);
